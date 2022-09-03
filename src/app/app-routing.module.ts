ff4411cf102749a20c49a1304a9908c33e3409ec
AlexTodor <alex_todor@yahoo.com>
2022-09-03 16:32:18 +0200

    added all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTasksComponent } from './add-tasks/add-tasks.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SummaryComponent } from './summary/summary.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent }
+  { path: '', component: LoginComponent},
+  { path: 'SignUp', component: SignUpComponent },
+  { path: 'ForgotPassword', component:  ForgotPasswordComponent},
+  { path: 'ResetPassword', component: ResetPasswordComponent },
+  { path: 'Summary', component: SummaryComponent },
+  { path: 'Board', component: BoardComponent},
+  { path: 'AddTasks', component: AddTasksComponent},
+  { path: 'Contacts', component: ContactsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { AddTasksComponent } from './add-tasks/add-tasks.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    SummaryComponent,
+    BoardComponent,
+    AddTasksComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
