e7fb10e069681e9fb4758a50c3102e0f84ec7f6f
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-08 15:24:56 +0200

    all task service methods created

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -8,13 +8,47 @@ import { Task } from 'src/models/task';
 })
 export class TaskService {
 
-  private url: string = "https://vicentbotellaferragud.pythonanywhere.com/api/task-list/";
+  private baseUrl: string = "https://vicentbotellaferragud.pythonanywhere.com/api";
 
   constructor(private http: HttpClient) { }
 
   public getAllTaks(): Observable<Task[]> {
 
-    return this.http.get<Task[]>(this.url);
+    const taskListEndpoint = "task-list/";
+
+    return this.http.get<Task[]>(`${this.baseUrl}/${taskListEndpoint}`);
+
+  }
+
+  public getTaskById(id: any): Observable<Task> {
+
+    const taskToGetEndpoint = `task-detail/${id}`;
+
+    return this.http.get<Task>(`${this.baseUrl}/${taskToGetEndpoint}`);
+
+  }
+
+  public createTask(task: Task): Observable<Task[]> {
+
+    const taskCreationEndpoint = "task-create/";
+    
+    return this.http.post<Task[]>(`${this.baseUrl}/${taskCreationEndpoint}`, task);
+
+  }
+
+  public updateTask(id: number, task: Task): Observable<Task[]> {
+
+    const taskToUpdateEndpoint = `task-update/${id}`;
+
+    return this.http.put<Task[]>(`${this.baseUrl}/${taskToUpdateEndpoint}`, task);
+
+  }
+
+  public deleteTask(id: number): Observable<Task[]> {
+
+    const taskToDeleteEndpoint = `task-delete/${id}`;
+
+    return this.http.delete<Task[]>(`${this.baseUrl}/${taskToDeleteEndpoint}`);
 
   }
 
