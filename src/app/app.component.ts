51c346fa8a000eb3c5ad307bf753f5dcff811656
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-14 23:37:02 +0200

    app.component.ts and login.component.ts files commented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/services/authentication.service';
 import { HotToastService } from '@ngneat/hot-toast';
@@ -9,35 +9,32 @@ import { HotToastService } from '@ngneat/hot-toast';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  disableClose: any;
 
-  title = 'Join';
+  title: string = 'Join';
 
-
-  constructor(private router: Router, private auth: AuthenticationService, private toast: HotToastService) { }
+  constructor(
+    private router: Router, 
+    private auth: AuthenticationService, 
+    private toast: HotToastService
+    ) { }
 
   ngOnInit(): void { }
 
+  /**
+   * Checks the current URL and returns "false" if it is "/login" or "/signup" or "true" if it is neither of those.
+   * @returns - true or false, depending on the currentPath variable.
+   */
   checkNotLoginOrSignup() {
 
     const currentPath = this.router.url;
-
-    if (currentPath === '/login' || currentPath === '/signup') {
-
-      this.disableClose = false;
-
-      return false;
-
-    } else {
-      
-      this.disableClose = true;
-
-      return true;
-      
-    }
+    return currentPath === '/login' || currentPath === '/signup' ? false : true;
 
   }
 
+  /**
+   * Logs out the user by calling the deleteCurrentUser function from the authentication service and displays a successful logout
+   * message. 
+   */
   logout() {
 
     this.auth.deleteCurrentUser();
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,30 +30,42 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void { }
 
+  /**
+   * Gets the username form control.
+   */
   get username() {
 
     return this.loginForm.get('username');
 
   }
 
+  /**
+   * Gets the password form control.
+   */
   get password() {
 
     return this.loginForm.get('password');
 
   }
 
+  /**
+   * Calls the getAllUsers function from the user service to get all the users from the server. Then, it looks for the user object that
+   * matches the passed-in data and logs in (or not) the user.
+   */
   submit() {
 
     this.userService.getAllUsers().pipe(takeUntil(this.destroy)).subscribe((data: User[]) => {
 
       const userFromTheDb = data.find((user: User) => user.username === this.username?.value && user.password === this.password?.value);
 
+      //If there is a user object in the database that matches the passed-in data...
       if (userFromTheDb) {
 
         this.authService.currentUser = userFromTheDb;
         this.authService.saveCurrentUser();
         this.successfulLogin();
 
+      //If there is NO user object in the database that matches the passed-in data...
       } else {
 
         this.unsuccessfulLogin();
@@ -64,6 +76,9 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Displays a successful login message and navigates the user to the summary view.
+   */
   successfulLogin() {
 
     this.toast.success('Logged in succesfully!');
@@ -71,21 +86,30 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Displays an unsuccessful login message.
+   */
   unsuccessfulLogin() {
 
     this.toast.error('Invalid username or password.');
 
   }
 
+  /**
+   * Does exactly the same as the submit function but with hard coded data.
+   */
   loginAsGuest() {
 
     const guest: User = new User();
 
+    //Hard coded data.
     guest.username = "guest";
     guest.password = "guest";
 
     this.userService.getAllUsers().pipe(takeUntil(this.destroy)).subscribe((data: User[]) => {
 
+      //There is a "guest" user in the server that cannot be deleted (from the frontend). Meaning: the function will always find
+      //the user object that matches the hard coded data.
       const guestFromDb = data.find((user: User) => user.username === guest.username && user.password === guest.password);
 
       if (guestFromDb) {
@@ -94,6 +118,8 @@ export class LoginComponent implements OnInit, OnDestroy {
         this.authService.saveCurrentUser();
         this.successfulLogin();
 
+      //Since the function will always find the user object that matches the hard coded data, this is not really necessary. It could
+      //be removed in the future.
       } else {
 
         this.toast.error('Log in as guest is not possible yet.');
@@ -104,6 +130,9 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Sets the local variable "destroy" to "true" so that all observables in the component are unsubscribed when this is "destroyed".
+   */
   ngOnDestroy(): void {
     
     this.destroy.next(true);
