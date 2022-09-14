b8146a5f6916740de6b79213b8c746ad25c39997
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-15 00:10:19 +0200

    sign-up.component.tsfile comments added

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -29,29 +29,44 @@ export class SignUpComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void { }
 
+  /**
+   * Gets the username form control.
+   */
   get username() {
 
     return this.signupForm.get('username');
 
   }
 
+  /**
+   * Gets the password form control.
+   */
   get password() {
 
     return this.signupForm.get('password');
 
   }
 
+  /**
+   * Gets the confirmPassword form control.
+   */
   get confirmPassword() {
 
     return this.signupForm.get('confirmPassword');
 
   }
 
+  /**
+   * Checks if the password and confirmPassword form controls have the same value (but only if they are not empty) and if so, it
+   * returns true. The function returns false if these two form controls have different values.
+   * @returns - true or false, depending on wether the password and confirmPassword form controls have the same value or not.
+   */
   checkPasswordsMatch() {
 
     const passwordCurrentValue = this.password?.value;
     const confirmPasswordCurrentValue = this.confirmPassword?.value;
 
+    //Only checks for match if both the password and confirmPassword form controls have some value.
     if (passwordCurrentValue.length && confirmPasswordCurrentValue.length) {
 
       return this.password?.value === this.confirmPassword?.value;
@@ -64,19 +79,24 @@ export class SignUpComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * By calling the createUser function from the user service creates a new user with the passed-in data or not.
+   */
   submit() {
 
-    const newUser = new User();
+    const newUser: User = new User();
 
     newUser.username = this.username?.value;
     newUser.password = this.password?.value;
 
     this.userService.createUser(newUser).pipe(takeUntil(this.destroy)).subscribe((data: User[] | string) => {
 
+      //If the username from the passed-in data matches any username from the server, no user is created.
       if (data === `There is already one user with the username '${newUser.username}'.`) {
         
         this.unsuccessfulResgistration();
 
+      //But, if the username from the passed-in data does not match any username from the server, a new user is created.
       } else {
 
         this.successfulResgistration();
@@ -87,6 +107,9 @@ export class SignUpComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Displays a successful registration message and navigates the user to the login view.
+   */
   successfulResgistration() {
 
     this.toast.success('New user succesfully created!');
@@ -94,12 +117,18 @@ export class SignUpComponent implements OnInit, OnDestroy {
 
   }
 
+  /**
+   * Displays an unsuccessful registration message.
+   */
   unsuccessfulResgistration() {
 
     this.toast.error(`There is already one user with the username '${this.username?.value}'.`);
 
   }
 
+  /**
+   * Sets the local variable "destroy" to "true" so that all observables in the component are unsubscribed when this is "destroyed".
+   */
   ngOnDestroy(): void {
 
     this.destroy.next(true);
