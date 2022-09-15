96ff7d205b9e87a91ec2a317d734374e85ecfc74
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-09-15 20:14:11 +0200

    more changes

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { User } from 'src/models/user';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  users: User[] = [];
 
+  destroy = new Subject();
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  /**
+   * Gets all the users from the server by calling the getAllUsers function from the user service. It then excludes the "guest" user
+   * object from the data (this is done because we do not want the "guest" user to be displayed, edited or deleted).
+   */
   ngOnInit(): void {
+
+    this.userService.getAllUsers().pipe(takeUntil(this.destroy)).subscribe((data: User[]) => {
+
+      const usersExcludingGuest = data.filter(e => e.username !== "guest");
+      this.users = usersExcludingGuest;
+
+    });
+
+  }
+
+  /**
+   * Sets the local variable "destroy" to "true" so that all observables in the component are unsubscribed when this is "destroyed".
+   */
+  ngOnDestroy(): void {
+
+    this.destroy.next(true);
+
   }
 
 }
